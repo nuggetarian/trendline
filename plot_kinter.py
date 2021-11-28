0e8bf2fc08c0d993825f65e9aac29fcf2f1a8a0d
nuggetarian <michal.zernovic@gmail.com>
2021-11-28 13:43:17 +0100

    Rozrobena equation

--- plot_kinter.py
+++ plot_kinter.py
@@ -49,9 +49,37 @@ def mainWindow():
         z = np.polyfit(x, y, int(zadanie))
         p = np.poly1d(z)
         plt.plot(x,p(x),"b:")
-        print(z)
+        #print(z)
 
-        zLabel = ttk.Label(window, text=z, font="Helvetica 12 bold").pack(pady=5)
+        z = str(z)
+        equation = []
+        equation = z.split(" ")
+        equation.remove('')
+        for i in range(len(equation)): 
+            equation[i] = str(equation[i])
+            i += 1
+
+
+        """f = len(equation)-1
+        for i in range(len(equation)): 
+            if i+1 == len(equation):
+                print(equation[i]) 
+            else:
+                print(equation[i]+"x"+str(f)) 
+            f -= 1
+            i += 1"""
+
+        f = len(equation)-1
+        for i in range(len(equation)): 
+            if i+1 == len(equation):
+                equation[i] 
+            else:
+                equation[i]+"x"+str(f)
+            f -= 1
+            i += 1
+
+
+        zLabel = ttk.Label(window, text=equation, font="Helvetica 12 bold").pack(pady=5)
 
         plt.legend()
         plt.grid()
@@ -61,3 +89,7 @@ def mainWindow():
 
 mainWindow()
 window.mainloop() 
+
+# Test Data
+#x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+#y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
